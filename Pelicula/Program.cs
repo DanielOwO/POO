92ce6849c824d16ec031724f108c17e9e8308591
DanielOwO <60365647+DanielOwO@users.noreply.github.com>
2020-02-21 20:49:34 -0800

    uwu

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Pelicula
 {
+    class Pelicula
+    {
+        public string titulo;
+        public Int16 año;
+        public string pais;
+        public string director;
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pelicula p1 = new Pelicula();
+            Pelicula p2 = new Pelicula();
+            p1.titulo = "Luz de luna";
+            p2.titulo = "Birdman";
+            p1.args = 2016;
+            p2.año = 2014;
+            p1.pais = "Estados Unidos";
+            p1.director = "Barry Jenkins";
+            p2.pais = "Estados Unidos";
+            p2.director = "Alejandro Gonzalez Iñarritu";
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # POO
 
 [Actividad 1]( ./Setup/README.md)
+
+[Actividad 2](./Pelicula/Program.cs)
