1765ce9fb221e0c35a8ca8b4283bf29b4a64454e
Daniel <60365647+DanielOwO@users.noreply.github.com>
2020-03-08 21:26:09 -0700

    owo

---
+++ Actores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Actores
+{
+    class Pelicula
+    {
+        private string actores;
+        public AgregarActor(string actores)
+        {
+            this.actores = actores;
+            Actor = new List<Actors>();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Pelicula p1 = new Pelicula("La La Land", 2016);
+           p1.AgregarActor(new Actor("Ryan Gosling", 1980));
+           p1.AgregarActor(new Actor("Emma Stone", 1988));
+           p1.AgregarActor(new Actor("Emma Stone", 1988));
+           p1.ImprimeActores();
+        }
+    }
+}
