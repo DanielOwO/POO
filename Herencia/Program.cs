1926dc0976d67d9a1ce8052c23aaa18e43d32384
Daniel <60365647+DanielOwO@users.noreply.github.com>
2020-04-02 19:48:24 -0700

    :3

---
+++ Herencia/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Herencia
+{
+    class Alumno
+    {
+        protected string name;
+        protected string ServicioSocial;
+        protected string Residencia;
+        protected string Investigacion;
+        public Alumno (string n)
+        {
+            name= n;
+        } 
+        public virtual void Presentate()
+        {
+            Console.WriteLine("Mi nombre es: {0}", name);
+        }
+    }
+    class Licenciatura:Alumno
+    {
+        protected string lic;
+        public Licenciatura(string name, String lic):base(name)
+        {
+            this.lic=lic;
+        }
+        public new void ServicioSocial()
+        {
+             Console.WriteLine("Este es mi servicio social: {0}",lic);
+        }
+        public new void Residencia()
+        {
+             Console.WriteLine("Esta es mi Residencia: {0}", lic);
+        } 
+    }
+    class Posgrado:Alumno
+    {
+        protected string pos;
+        public Posgrado(string name, String pos):base(name)
+        {
+            this.pos=pos;
+        }
+        public new void Investigacion()
+        {
+            Console.WriteLine("Mi invetigacion es: {0}",pos);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno Ricardo = new Alumno("Ricardo");
+            Ricardo.Presentate();
+            Licenciatura Elizabeth = new Licenciatura("Elizabeth","Doc1");
+            Elizabeth.Presentate();
+            Elizabeth.ServicioSocial();
+            Elizabeth.Residencia();
+            Posgrado Ximena =new Posgrado("Ximena","Doc1");
+            Ximena.Presentate();
+            Ximena.Investigacion();
+        }
+    }
+}
---
+++ Musicos/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Musicos
+{
+    class Musico
+    {
+        protected string nombre;
+        protected string Afinar;
+        protected string Toque;
+        public Musico (string n)
+        {
+            nombre = n;
+        } 
+        public virtual void saluda()
+        {
+            Console.WriteLine("Hola soy {0}",nombre);
+        }
+        public virtual void Afina()
+        {
+            Console.WriteLine("dejen afino ");
+        }
+        public virtual void Tocar()
+        {
+            Console.WriteLine("Vamos a empezar a tocar");
+        }
+    }
+    class Baterista:Musico
+    {
+        protected string bateria;
+        public Baterista(string nombre, string bateria):base(nombre)
+        {
+            this.bateria= bateria; 
+        }
+         public override void saluda()
+        {
+            Console.WriteLine("Hola soy El Baterista {0}, mi bateria el la mejor, es una {1}",nombre, bateria);
+        }
+         public override void Afina()
+        {
+            Console.WriteLine("voy a calar la bateria");
+        }
+        public override void Tocar()
+        {
+            Console.WriteLine("Empecemos con mi solo");
+        }
+    }
+    class Bajista:Musico
+    {
+        protected string bajo;
+        public Bajista(string nombre, string bajo):base(nombre)
+        {
+            this.bajo= bajo; 
+        }
+         public override void saluda()
+        {
+            Console.WriteLine("Hola soy El bajista {0}, toco bien perro mi bajo, es un {1}",nombre, bajo);
+        }
+        public override void Afina()
+        {
+            Console.WriteLine("voy a afinar mi bajo");
+        }
+         public override void Tocar()
+        {
+            Console.WriteLine("Empecemos con mi solo");
+        }
+    }
+    class Guitarrista:Musico
+    {
+        protected string Guitarra;
+        public Guitarrista(string nombre, string Guitarra):base(nombre)
+        {
+            this.Guitarra= Guitarra; 
+        }
+         public override void saluda()
+        {
+            Console.WriteLine("Hola soy El Guitarrista {0},escucha este solo de mi {1}",nombre, Guitarra);
+        }
+        public override void Afina()
+        {
+            Console.WriteLine("voy a afinar mi");
+        }
+         public override void Tocar()
+        {
+            Console.WriteLine("Empecemos con mi solo");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Musico Marcial = new Musico(" Marcial ");
+           Marcial.saluda();
+           Marcial.Afina();
+           Marcial.Tocar();
+            Baterista Javier = new Baterista("Javier", "tommy");
+            Bajista Tom = new Bajista("Tom", "pilar ");
+            Guitarrista Donald= new Guitarrista("Donald", "P34k ");
+            Javier.saluda();
+            Javier.Afina();
+            Javier.Tocar();
+            Tom.saluda();
+            Tom.Afina();
+            Tom.Tocar();
+            Donald.saluda();
+            Donald.Afina();
+            Donald.Tocar();
+        List<Musico> Musicos = new List<Musico>();
+            Musicos.add(Marcial);
+            Musicos.add(Javier);
+            Musicos.add(Tom);
+            Musicos.add(Donald);
+        foreach(Musico n in Musicos)
+        {
+            n.saluda();
+        }
+        }
+    }
+}
