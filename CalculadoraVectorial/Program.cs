c1e8a44685d531ec6abbda7427820ed4fcc01820
Daniel <60365647+DanielOwO@users.noreply.github.com>
2020-04-03 19:23:28 -0700

    ok

---
+++ CalculadoraVectorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculadoraVectorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
