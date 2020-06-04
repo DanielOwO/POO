f28ae48c728a5104d9aa808bd97d1b3146c994d7
Daniel <60365647+DanielOwO@users.noreply.github.com>
2020-06-04 12:25:58 -0700

    n

---
+++ Proyecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Proyecto
+{
+    class Producto
+    {
+        private codigo;
+    }
+
+    class ProductoDB
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
