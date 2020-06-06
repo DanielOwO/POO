7819894161d50b0963ce0b23b8791701bf7ccb19
Daniel <60365647+DanielOwO@users.noreply.github.com>
2020-06-06 13:31:51 -0700

    x

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -4,17 +4,38 @@ using System.IO;
 
 namespace Proyecto
 {
+    //Declaracion de la clase producto
     class Producto
     {
-        protected string codigo;      
-        protected string descripcion;
-        protected int precios;
-        protected int likes;
+        //Constructor
+        public Producto(string c, string d)
+        {
+            codigo = c;
+            descripcion = d;
+
+        }
+        public string codigo{get; set;}   
+        public string descripcion{get; set;}
+        List<PrecioFecha> precios;
+        public int likes;
+        public void getPrecio()
+        {
+            
+        }
     }
+    //Declaracion de la clase ProductoDB
 
     class ProductoDB
     {
-
+        public static void WriteToTXT(string path, List<Producto> productos)
+        {
+            StreamWriter txtOut = new StreamWriter(new FileStream(path, FileMode.Create, FileAccess.Write));
+            foreach(Producto p in productos)
+            {
+                txtOut.WriteLine("{0}|{1}", p.codigo, p.descripcion);
+            }
+            txtOut.Close();
+        }
     }
     class PrecioFecha
     {
@@ -26,7 +47,13 @@ namespace Proyecto
     {
         static void Main(string[] args)
         {
+            //Declaracion de la lista productos
             List<Producto> productos = new List<Producto>();
+            productos.Add(new Producto("0000", "Nintendo DSi XL"));
+            productos.Add(new Producto("0001", "Samsung Galaxy S10+"));
+            productos.Add(new Producto("0002", "GTX GeForce Nvidia 2080Ti"));
+            //Llamada al procedimiento writetoTXT
+            ProductoDB.WriteToTXT("productos.txt", productos);
             Console.WriteLine("Hello World!");
         }
     }
